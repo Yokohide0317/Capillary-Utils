cb84b50b2dd75fa4b43678fa8a33da4154e51703
Yokohide0317 <yokohide0317@gmail.com>
2024-03-07 23:22:53 +0900

    update for Docker

---
+++ README.md
@@ -0,0 +1,12 @@
+# Capillary-Utils
+
+
+## Build
+```
+docker build . --tag capillary-web:latest
+```
+
+## Run
+```
+docker run --rm -v $(pwd):/app -p 8501:8501 capillary-web:latest
+```
---
+++ makeall_to_fasta.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import os
+from pathlib import Path
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import numpy as np
+import pandas as pd
+from Bio import SeqIO
+
